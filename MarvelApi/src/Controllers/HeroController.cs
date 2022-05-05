c6ffc4c218581d711250b30c284e45b8c4d27a35
Ricardo <Ricardo@NOTE-DEV>
2022-05-05 01:18:03 -0300

    Adicionar arquivos de projeto.

---
+++ MarvelApi/src/Controllers/HeroController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers
+{
+    [ApiController]
+    [Route("api/v1/Hero")]
+    public class HeroController
+    {
+        [HttpGet]
+      public string Create()
+        {
+            return "Teste API";
+        }
+    }
+}
---
+++ MarvelApi/src/Models/Hero.cs
@@ -0,0 +1,16 @@
+﻿namespace src.Models
+{
+    public class Hero
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string RealName { get; set; }
+        public DateTime CreatedAt   { get; set; }
+
+        public int GroupId { get; set; }
+        public Group group { get; set; }
+
+
+
+    }
+}
